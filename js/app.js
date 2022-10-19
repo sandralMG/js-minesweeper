46442327f2d09f82b8aa76eae03eab68e77d1548
mediagymnasiet-webbutveckling <88836988+sandra-ntig@users.noreply.github.com>
2022-10-19 07:36:51 +0200

    start

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const gamearea = document.querySelector(".gamearea");
+
+let bombAmount = 20;
+let size = 20;
+let squares = [];
+let gameOver = false;
+
+function createBoard() {
+    
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(size * size - bombAmount).fill('valid');
+    const gameArray = emptyArray.concat(bombsArray);
+    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+
+    console.log("shuffled array", shuffledArray);
+
+
+    for(let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("id", i);
+        gamearea.appendChild(square);
+        squares.push(square);
+        console.log("test");
+    }
+
+    square.addEventListener('click', function(e) {
+         let currentId = square.id;
+         if(isGameOver) return;
+         if(square.classList.contains('checked') || square.classList.contains('flagged')) return;
+         if((square.classList.contains('bomb'))) {
+            gameOver(square);
+         } else {
+             let total = square.getAttribute('data');
+             square.classList.add('checked')
+             if(total === 1) square.classList.add('one');
+             if(total === 2) square.classList.add('two');
+             if(total === 3) square.classList.add('three');
+             if(total === 4) square.classList.add('four');
+            square.innerHTML = total;
+            return
+         }
+    })
+
+}
+
+createBoard();
+console.log(squares);
